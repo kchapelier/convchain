140fc711f9ec9601f311a690047fb4019e4fe28b
kchapelier <kevin.chapelier@gmail.com>
2016-08-21 12:45:34 +0200

    proper cjs, api cleanup, style cleanup, remove ES6 things, support non-square output, doc, ...

--- README.md
+++ README.md
@@ -1,3 +1,109 @@
 # convchain
 
-(work in progress) Js port of https://github.com/mxgmn/ConvChain
+Vanilla javascript port of [ConvChain](https://github.com/mxgmn/ConvChain).
+
+## Installing and testing
+
+With [npm](http://npmjs.org) do:
+
+```
+npm install convchain
+```
+
+## Basic example
+
+```js
+var ConvChain = require('convchain');
+
+var samplePattern = Uint8Array.from([
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 0, 0, 0, 0, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 1, 0, 0, 0, 0, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+]);
+
+var width = 45,
+    height = 20;
+
+var convChain = new ConvChain(samplePattern);
+
+var generatedPattern = convChain.generate([width, height], 3, 0.5, 4); // a flat Uint8Array
+
+// some code to display the result
+for (var y = 0; y < height; y++) {
+    var s = '';
+    for (var x = 0; x < width; x++) {
+        s += ' ' + generatedPattern[x + y * width];
+    }
+    console.log(s);
+}
+```
+
+## Public API
+
+### Constructor
+
+**new ConvChain(sample[, sampleSize])**
+
+ - *sample :* Sample pattern as a flat array or a 2D array.
+ - *sampleSize :* Indicate the width and height of the sample when used with a flat array, if omitted the sample pattern is assumed to be a square.
+
+```js
+var testSample = Uint8Array.from([
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+]); //flat array
+
+var convChain = new ConvChain(testSample, [14, 10]);
+```
+
+```js
+var testSample = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]; //2D array
+
+var convChain = new ConvChain(testSample);
+```
+
+### Methods
+
+**convChain.setSample(sample[, sampleSize])**
+
+Same arguments as the constructor
+
+**convChain.generate(resultSize, n, temperature, iterations[, rng])**
+
+Generate a new pattern based on the sample pattern. The generated pattern is returned as a flat Uin8Array.
+
+ - *resultSize :* Width and height of the generated pattern.
+ - *n :* Receptor size, an integer greater than 0.
+ - *temperature :* Temperature, a value between 0 and 1.
+ - *iterations :* Number of iterations.
+ - *rng :* A function to use as random number generator, defaults to Math.random.
+
+```js
+convChain.generate([80, 80], 3, 0.5, 4);
+```
--- index.js
+++ index.js
@@ -1,21 +1,75 @@
 "use strict";
 
-var ConvChain = function ConvChain(source) {
-    this.source = source;
+/**
+ * ConvChain constructor
+ * @param {Array|Uint8Array} sample Sample pattern as a flat array or a 2D array
+ * @param {int|Array} [sampleSize] Indicate the width and height of the sample when used with a flat array, if omitted assume the sample is a square
+ * @constructor
+ */
+var ConvChain = function ConvChain (sample, sampleSize) {
+    this.setSample(sample, sampleSize);
 };
 
-ConvChain.prototype.execute = function (n, temperature, size, iterations, rng) {
-    rng = rng || Math.random;
+/**
+ * Set the sample pattern
+ * @param {Array|Uint8Array} sample Sample pattern as a flat array or a 2D array
+ * @param {int|Array} [sampleSize] When used with a flat array indicate the width and height of the sample, if omitted assume the sample is a square
+ */
+ConvChain.prototype.setSample = function (sample, sampleSize) {
+    if (typeof sample[0] === 'number') {
+        // assume flat array
+        this.sample = sample;
 
-    var sample = this.source;
+        if (!sampleSize) {
+            // assume square sample
 
-    var field = new Array(size);
+            this.sampleWidth = this.sampleHeight = Math.sqrt(sample.length) | 0;
+        } else {
+            this.sampleWidth = typeof sampleSize === 'number' ? sampleSize : sampleSize[0];
+            this.sampleHeight = typeof sampleSize === 'number' ? sampleSize : sampleSize[1];
+        }
+    } else {
+        // assume 2D array
+        this.sampleWidth = sample[0].length;
+        this.sampleHeight = sample.length;
 
-    for (var i = 0; i < size; i++) {
-        field[i] = new Array(size);
+        var flatArray = new Uint8Array(this.sampleWidth * this.sampleHeight),
+            x,
+            y;
+
+        for (y = 0; y < this.sampleHeight; y++) {
+            for (x = 0; x < this.sampleWidth; x++) {
+                flatArray[x + y * this.sampleWidth] = sample[y][x];
+            }
+        }
+
+        this.sample = flatArray;
     }
+};
 
-    var weights = new Float32Array(1 << (n * n));
+/**
+ * Generate a pattern based on the sample pattern
+ * @param {int|Array} resultSize Width and height of the generated pattern
+ * @param {int} n Receptor size, an integer greater than 0
+ * @param {float} temperature Temperature, a value between 0 and 1
+ * @param {int} iterations Number of iterations
+ * @param {function} [rng] A random number generator, default to Math.random
+ * @returns {Uint8Array} Generated pattern, returned as a flat Uint8Array
+ */
+ConvChain.prototype.generate = function (resultSize, n, temperature, iterations, rng) {
+    var sample = this.sample,
+        sampleWidth = this.sampleWidth,
+        sampleHeight = this.sampleHeight,
+        resultWidth = typeof resultSize === 'number' ? resultSize : resultSize[0],
+        resultHeight = typeof resultSize === 'number' ? resultSize : resultSize[1],
+        field = new Uint8Array(resultWidth * resultHeight),
+        weights = new Float32Array(1 << (n * n)),
+        k,
+        x,
+        y;
+
+    n = Math.max(n, 1);
+    rng = rng || Math.random;
 
     var pattern = function pattern (fn) {
         var result = new Array(n * n),
@@ -32,11 +86,11 @@ ConvChain.prototype.execute = function (n, temperature, size, iterations, rng) {
     };
 
     var rotate = function rotate (p) {
-        return pattern((x, y) => p[n - 1 - y + x * n]);
+        return pattern(function (x, y) { return p[n - 1 - y + x * n]; });
     };
 
     var reflect = function reflect (p) {
-        return pattern((x, y) => p[n - 1 - x + y * n]);
+        return pattern(function (x, y) { return p[n - 1 - x + y * n]; });
     };
 
     var index = function index (p) {
@@ -52,21 +106,16 @@ ConvChain.prototype.execute = function (n, temperature, size, iterations, rng) {
         return result;
     };
 
-    var k, x, y;
-
-    var sampleWidth = sample[0].length,
-        sampleHeight = sample.length;
-
     for (y = 0; y < sampleHeight; y++) {
         for (x = 0; x < sampleWidth; x++) {
-            var p0 = pattern((dx, dy) => sample[(x + dx) % sampleWidth][((y + dy) % sampleHeight)]);
-            var p1 = rotate(p0);
-            var p2 = rotate(p1);
-            var p3 = rotate(p2);
-            var p4 = reflect(p0);
-            var p5 = reflect(p1);
-            var p6 = reflect(p2);
-            var p7 = reflect(p3);
+            var p0 = pattern(function (dx, dy) { return sample[((x + dx) % sampleWidth) + ((y + dy) % sampleHeight) * sampleWidth]; }),
+                p1 = rotate(p0),
+                p2 = rotate(p1),
+                p3 = rotate(p2),
+                p4 = reflect(p0),
+                p5 = reflect(p1),
+                p6 = reflect(p2),
+                p7 = reflect(p3);
 
             weights[index(p0)] += 1;
             weights[index(p1)] += 1;
@@ -80,40 +129,62 @@ ConvChain.prototype.execute = function (n, temperature, size, iterations, rng) {
     }
 
     for (k = 0; k < weights.length; k++) {
-        if (weights[k] <= 0) weights[k] = 0.1;
-    }
-
-    for (y = 0; y < size; y++) {
-        for (x = 0; x < size; x++) {
-            field[x][y] = rng() < 0.5;
+        if (weights[k] <= 0) {
+            weights[k] = 0.1;
         }
     }
 
-    for (k = 0; k < iterations * size * size; k++) {
-        var r = (rng() * size * size) | 0;
-        x = (r % size) | 0;
-        y = (r / size) | 0;
+    for (y = 0; y < resultHeight; y++) {
+        for (x = 0; x < resultWidth; x++) {
+            field[x + y * resultWidth] = rng() < 0.5;
+        }
+    }
 
-        var q = 1;
+    for (k = 0; k < iterations * resultWidth * resultHeight; k++) {
+        var r = (rng() * resultWidth * resultHeight) | 0,
+            q = 1,
+            sy,
+            sx,
+            dy,
+            dx,
+            ind,
+            difference;
 
-        for (var sy = y - n + 1; sy <= y + n - 1; sy++) {
-            for (var sx = x - n + 1; sx <= x + n - 1; sx++) {
-                var ind = 0,
-                    difference = 0;
+        x = (r % resultWidth) | 0;
+        y = (r / resultWidth) | 0;
 
-                for (var dy = 0; dy < n; dy++) for (var dx = 0; dx < n; dx++) {
-                    var X = sx + dx;
-                    if (X < 0) X += size;
-                    else if (X >= size) X -= size;
+        for (sy = y - n + 1; sy <= y + n - 1; sy++) {
+            for (sx = x - n + 1; sx <= x + n - 1; sx++) {
+                ind = 0;
+                difference = 0;
 
-                    var Y = sy + dy;
-                    if (Y < 0) Y += size;
-                    else if (Y >= size) Y -= size;
+                for (dy = 0; dy < n; dy++) {
+                    for (dx = 0; dx < n; dx++) {
+                        var power = 1 << (dy * n + dx),
+                            X = sx + dx,
+                            Y = sy + dy,
+                            value;
 
-                    var value = field[X][Y];
-                    var power = 1 << (dy * n + dx);
-                    ind += value ? power : 0;
-                    if (X == x && Y == y) difference = value ? power : -power;
+                        if (X < 0) {
+                            X += resultWidth;
+                        } else if (X >= resultWidth) {
+                            X -= resultWidth;
+                        }
+
+                        if (Y < 0) {
+                            Y += resultHeight;
+                        } else if (Y >= resultHeight) {
+                            Y -= resultHeight;
+                        }
+
+                        value = field[X + Y * resultWidth];
+
+                        ind += value ? power : 0;
+
+                        if (X === x && Y === y) {
+                            difference = value ? power : -power;
+                        }
+                    }
                 }
 
                 q *= weights[ind - difference] / weights[ind];
@@ -122,7 +193,7 @@ ConvChain.prototype.execute = function (n, temperature, size, iterations, rng) {
 
 
         if (q >= 1) {
-            field[x][y] = !field[x][y];
+            field[x + y * resultWidth] = !field[x + y * resultWidth];
             continue;
         }
 
@@ -131,48 +202,11 @@ ConvChain.prototype.execute = function (n, temperature, size, iterations, rng) {
         }
 
         if (q > rng()) {
-            field[x][y] = !field[x][y];
+            field[x + y * resultWidth] = !field[x + y * resultWidth];
         }
     }
 
     return field;
 };
 
-var testSample = [
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0],
-    [0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0],
-    [0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0],
-    [0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0],
-    [0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0],
-    [0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0],
-    [0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0],
-    [0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0],
-    [0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0],
-    [0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0],
-    [0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0],
-    [0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0],
-    [0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0],
-    [0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0],
-    [0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-];
-
-
-var time = Date.now();
-
-var size = 15;
-var conv = new ConvChain(testSample);
-
-var result = conv.execute(3, 0.5, size, 50);
-
-result.forEach((v) => {
-    var s = '';
-    v.map(v => { s = s + (v ? 'O' : '.') + ' '; });
-    console.log(s);
-});
-
-console.log(Date.now() - time);
+module.exports = ConvChain;
